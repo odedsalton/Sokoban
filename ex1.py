0e016a469aba9adfe870d73564a85c902b1d7de7
Sean <shawn.yog@gmail.com>
2018-12-03 13:26:52 +0200

    @Sean

--- ex1.py
+++ ex1.py
@@ -7,51 +7,9 @@ ids = ["203703467", "204375687"]
 
 """
 *******************************************************************************************************************
-My class for help
+Classes for help
 *******************************************************************************************************************
 """
-
-
-class Helper:
-    """Helper functions"""
-
-    def __init__(self):
-        pass
-
-    def findPlayer(self, state):
-        for s in state:
-            for w in s:
-                if w in (17, 37):
-                    x = s.index(w)
-                    y = state.index(s)
-                    return tuple(x, y)
-
-    def canMove(self, state, x0, y0, x1, y1):
-        if state[x1, y1]:
-            # if not in game coordinate
-            if state[x1, y1] == 99:
-                # if a wall
-                return False
-            elif state[x1, y1] in (15, 35):
-                # if there is box we need to check the next coordinate also
-                return Helper.checkStone(state, x1 + (x1 - x0), y1 + (y1 - y0))
-            return True
-        return False
-
-    def checkStone(self, state, x2, y2):
-        if state[x2, y2] in [10, 20, 30]:
-            return True
-        return False
-
-# if state[x1, y1] in (99, None):
-        #     return False
-        # elif state[x1, y1] in (15, 35):
-        #     if state[x2, y2] in (99, 35, 15, None):
-        #         return False
-        #     return True
-        # return True
-
-
 
 #
 # class Spot:
@@ -84,6 +42,52 @@ class Helper:
 #
 #     def __str__(self):
 #         return self.char
+#
+# """Global Directions"""
+# R = Direction(Spot(1, 0), 'R')
+# U = Direction(Spot(0, 1), 'U')
+# L = Direction(Spot(-1, 0), 'L')
+# D = Direction(Spot(0, -1), 'D')
+#
+#
+# class State:
+#     """class to represent a dynamic state of the board"""
+#
+#     def __init__(self, state):
+#
+#         pass
+
+
+class Helper:
+    """Helper functions"""
+
+    def __init__(self):
+        pass
+
+    def findPlayer(self, state):
+        for x, t in enumerate(state):
+            for y, v in enumerate(t):
+                if y in (17, 27, 37):
+                    return x, y
+
+    def canMove(self, state, x0, y0, x1, y1):
+        # if coordinate in game
+        if state[x1, y1]:
+            # if a wall, not correct because still can move but nothing happens! (according to instructions)
+            if state[x1, y1] == 99:
+                return False
+            # if there is box we need to check the next coordinate
+            elif state[x1, y1] in (15, 35):
+                return Helper.checkStone(state, x1 + (x1 - x0), y1 + (y1 - y0))
+            return True
+        return False
+
+    def checkStone(self, state, x2, y2):
+        # checks whether (x2,y2) is free to move a box to
+        if state[x2, y2] in [10, 20, 30]:
+            return True
+        return False
+
 
 """
 *******************************************************************************************************************
@@ -129,19 +133,18 @@ class SokobanProblem(search.Problem):
     def goal_test(self, state):
         """ Given a state, checks if this is the goal state.
          Returns True if it is, False otherwise."""
-
+        not_allowed = [15, 20, 27, 35]
         for s in state:
             for w in s:
-                if w in [15, 20, 27, 35]:
+                if w in not_allowed:
+                    # if any of those exists then there is a destination without box on it
                     return False
-
         return True
 
     def h(self, node):
         """ This is the heuristic. It gets a node (not a state,
         state can be accessed via node.state)
         and returns a goal distance estimate"""
-        pass
 
     """Feel free to add your own functions"""
 
